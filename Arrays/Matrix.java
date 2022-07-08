a852f3ff308f34b2424fd1abb9b7b5dc507523d4
Roland Gebe <roland.gebe@gmail.com>
2022-07-08 17:54:56 +0200

    update

--- Arrays/Matrix.java
+++ Arrays/Matrix.java
@@ -1,12 +1,16 @@
-package arrays;
-
 public class Matrix {
     public static void main(String[] args) {
         //int[][] aMatrix = new int[4][];
-        int[][] aMatrix = {{1, 2, 3, 4},{1, 2, 3, 4, 5}};
-        int[][] bMatrix = {{1, 2, 3, 4},{1, 2, 3, 4, 5}};
-        //{null, ..., null}
-        //{{0, 0, 0, 0, 0}, ..., {0, 0, 0, 0, 0}}
+        int[][] aMatrix = {
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5}};
+        int[][] bMatrix = {
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5}};
 
         int[][] addition = addMatrices(aMatrix, bMatrix);
         int[][] transposed = transpose(aMatrix);
@@ -18,24 +22,25 @@ public class Matrix {
     }
 
     private static int[][] addMatrices(int[][] aMatrix, int[][] bMatrix) {
-        int[][] cMatrix = new int[4][5];
+        int[][] cMatrix = new int[5][4];
         //sucet matic - kazdy prvok s kazdym prvkom
-        for (int i = 0; i < aMatrix.length; i++) {
-            for (int j = 0; j < bMatrix.length; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
                 cMatrix[i][j] = aMatrix[i][j] + bMatrix[i][j];
             }
 
         }
         //vysledok bude v matrixC
         printMatrix(cMatrix);
+        System.out.println();
         return cMatrix;
     }
 
     private static int[][] transpose(int[][] matrix) {
-        int[][] transposed = new int[4][5];
+        int[][] transposed = new int[5][4];
         //https://cs.wikipedia.org/wiki/Transpozice_matice
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
                 transposed[i][j] = matrix [j][i];
 
             }
@@ -43,18 +48,20 @@ public class Matrix {
         }
         //vysledok bude v matrixC
         printMatrix(transposed);
+        System.out.println();
         return transposed;
     }
 
     private static int[][] multiply(int[][] aMatrix, int[][] bMatrix) {
-        int[][] cMatrix = new int[4][5];
-        for (int i = 0; i < aMatrix.length; i++) {
-            for (int j = 0; j < bMatrix.length; j++) {
+        int[][] cMatrix = new int[5][4];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
                 cMatrix[i][j] = aMatrix[i][j] * bMatrix[i][j];
             }
 
         }
         printMatrix(cMatrix);
+        System.out.println();
         return cMatrix;
     }
 
