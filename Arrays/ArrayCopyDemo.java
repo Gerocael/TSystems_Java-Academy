72f71f122665b72f0bb45a1349e7163c6995d657
Roland Gebe <roland.gebe@gmail.com>
2022-07-08 09:58:25 +0200

    test

--- Arrays/ArrayCopyDemo.java
+++ Arrays/ArrayCopyDemo.java
@@ -1,10 +1,27 @@
 public class ArrayCopyDemo {
     public static void main(String[] args) {
-        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
-			    'i', 'n', 'a', 't', 'e', 'd' };
-        char[] copyTo = new char[7];
+        int[] copyFrom = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        int[] copyTo = new int[7];
+        int[] copyFrom2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        int[] copyTo2 = new int[7];
 
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
-        System.out.println(new String(copyTo));
+        for(int i : copyTo) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+
+        arraycopy(copyFrom2, 2, copyTo2, 0, 7);
+        for(int i : copyTo) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+    }
+
+    private static boolean arraycopy(int[] source, int srcIndex, int[] dest, int destIndex, int length) {
+
+
+
+        return false;
     }
 }
