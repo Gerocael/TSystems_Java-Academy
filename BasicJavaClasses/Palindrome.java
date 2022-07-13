498c9f9389bc99120a8b2aa982d9ba3b3e382dab
Roland Gebe <roland.gebe@gmail.com>
2022-07-13 21:43:41 +0200

    pal. up

--- BasicJavaClasses/Palindrome.java
+++ BasicJavaClasses/Palindrome.java
@@ -1,15 +1,13 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String palindrome = "Toto";
+        String palindrome = "Toot";
         int len = palindrome.length();
         String clean = palindrome.toLowerCase();
         StringBuilder dest = new StringBuilder(len);
-        for (int i = (len - 1); i >= 0; i--) {
-            dest.append(clean.charAt(i));
-        }
         StringBuilder dest2 = new StringBuilder(len);
         for (int i = (len - 1); i >= 0; i--) {
-            dest2.append(dest.charAt(i));
+            dest.append(clean.charAt(i));
+            dest2.append(clean.charAt(i));
         }
         if (dest.compareTo(dest2)!=0) {
             System.out.println("Not a Palindrome");
