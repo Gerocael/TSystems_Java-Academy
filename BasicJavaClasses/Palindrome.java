b0205e46f259107be5d4342b3296580b8ad61b30
Roland Gebe <roland.gebe@gmail.com>
2022-07-13 20:30:57 +0200

    palindrome

--- BasicJavaClasses/Palindrome.java
+++ BasicJavaClasses/Palindrome.java
@@ -1,16 +1,22 @@
-import static java.lang.StringUTF16.toLowerCase;
-
 public class Palindrome {
-	public static void main(String[] args) {
-		String palindrome = "jarko";
-		int len = palindrome.length();
-		StringBuilder dest = new StringBuilder(len);
-		for (int i = (len - 1); i >= 0; i--) {
-			dest.append(palindrome.charAt(i));
-		}
-		System.out.println(dest.toString());
-	}
-	public static boolean isPalindrome(String palindrome){
-		palindrome = toLowerCase();
-	}
+    public static void main(String[] args) {
+        String palindrome = "Toto";
+        int len = palindrome.length();
+        String clean = palindrome.toLowerCase();
+        StringBuilder dest = new StringBuilder(len);
+        for (int i = (len - 1); i >= 0; i--) {
+            dest.append(clean.charAt(i));
+        }
+        StringBuilder dest2 = new StringBuilder(len);
+        for (int i = (len - 1); i >= 0; i--) {
+            dest2.append(dest.charAt(i));
+        }
+        if (dest.compareTo(dest2)!=0) {
+            System.out.println("Not a Palindrome");
+        } else {
+            System.out.println("Is a Palindrome");
+        }
+        System.out.println(clean.toString());
+        System.out.println(dest.toString());
+    }
 }
