a3c60de7be7e30240d5bcaec98ab9f9c0fbce11e
Roland Gebe <roland.gebe@gmail.com>
2022-07-14 10:25:58 +0200

    pali. u.

--- BasicJavaClasses/Palindrome.java
+++ BasicJavaClasses/Palindrome.java
@@ -1,20 +1,30 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String palindrome = "Toot";
+        String palindrome = "abba";
         int len = palindrome.length();
-        String clean = palindrome.toLowerCase();
-        StringBuilder dest = new StringBuilder(len);
-        StringBuilder dest2 = new StringBuilder(len);
-        for (int i = (len - 1); i >= 0; i--) {
-            dest.append(clean.charAt(i));
-            dest2.append(clean.charAt(i));
-        }
-        if (dest.compareTo(dest2)!=0) {
-            System.out.println("Not a Palindrome");
-        } else {
-            System.out.println("Is a Palindrome");
-        }
-        System.out.println(clean.toString());
-        System.out.println(dest.toString());
+        System.out.println(isPalindrome(palindrome));
     }
+
+    public static boolean isPalindrome(String palindrome){
+        String clean = palindrome.toLowerCase();
+        String rev = new StringBuffer(clean).reverse().toString();
+        if(clean.equals(rev)){
+            return true;
+        }
+        return false;
+    }
+//        String clean = palindrome.toLowerCase();
+//        StringBuilder dest = new StringBuilder(len);
+//        StringBuilder dest2 = new StringBuilder(len);
+//        for (int i = (len - 1); i >= 0; i--) {
+//            dest.append(clean.charAt(i));
+//            dest2.append(clean.charAt(i));
+//        }
+//        if (dest.compareTo(dest2)!=0) {
+//            System.out.println("Not a Palindrome");
+//        } else {
+//            System.out.println("Is a Palindrome");
+//        }
+//        System.out.println(clean.toString());
+//        System.out.println(dest.toString());
 }
