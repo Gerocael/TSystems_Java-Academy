d3783c4be4be755a22a770dedc03bef4d8306b3e
Roland Gebe <roland.gebe@gmail.com>
2022-07-21 12:19:09 +0200

    miska update

--- IO/io/ReadingFromSystemIn.java
+++ IO/io/ReadingFromSystemIn.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 
 public class ReadingFromSystemIn {
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader input =
+                new BufferedReader(
+                        new InputStreamReader(System.in));
 
-        String s;        
+        String s;
         do {
             System.out.println("Enter text: ");
             s = input.readLine();
